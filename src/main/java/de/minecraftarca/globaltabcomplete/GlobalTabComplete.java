850e552e34e2e4bb751d6c3e4d6a691548e61ef3
Nathan Towell <towellnathan@gmail.com>
2014-12-06 23:29:49 +1030

    Forgot to extend and implement

--- src/main/java/de/minecraftarca/globaltabcomplete/GlobalTabComplete.java
+++ src/main/java/de/minecraftarca/globaltabcomplete/GlobalTabComplete.java
@@ -5,10 +5,12 @@ package de.minecraftarca.globaltabcomplete;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.TabCompleteEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
-public class GlobalTabComplete {
+public class GlobalTabComplete extends Plugin implements Listener {
 
     public ProxyServer getProxy() {
         return ProxyServer.getInstance();
