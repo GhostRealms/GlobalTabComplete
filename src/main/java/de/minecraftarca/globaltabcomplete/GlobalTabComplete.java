2c171e62b93f6b27493c257616caae2c90c64619
Nathan Towell <towellnathan@gmail.com>
2014-12-06 23:26:28 +1030

    Initial Commit

---
+++ src/main/java/de/minecraftarca/globaltabcomplete/GlobalTabComplete.java
@@ -0,0 +1,36 @@
+package de.minecraftarca.globaltabcomplete;
+
+// Copyright (c) 2014 MinecraftArca.de. All Rights Reserved.
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.TabCompleteEvent;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
+
+public class GlobalTabComplete {
+
+    public ProxyServer getProxy() {
+        return ProxyServer.getInstance();
+    }
+
+    public void onEnable() {
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onTab(TabCompleteEvent event) {
+        if (!event.getSuggestions().isEmpty()) {
+            return;
+        }
+        String[] args = event.getCursor().split(" ");
+
+        final String start = (args.length > 0 ? args[args.length - 1] : event.getCursor()).toLowerCase();
+
+        for (ProxiedPlayer player : getProxy().getPlayers()) {
+            if (player.getName().toLowerCase().startsWith(start)) {
+                event.getSuggestions().add(player.getName());
+            }
+        }
+    }
+
+}
